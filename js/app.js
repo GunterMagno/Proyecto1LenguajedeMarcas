3a610391e01c976bf5f4d5f1227659cb2b14a6c4
GunterMagno <bcalejandro@iesrafaelalberti.es>
2024-11-28 14:17:46 +0100

    inicio sesion configuracion colores css y un poco de responsive

--- js/app.js
+++ js/app.js
@@ -8,3 +8,13 @@ function toggleTheme() {
     document.getElementById('theme-icon').src="img/luna-rellena.png";
 }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const enlacesInteractivos = document.querySelectorAll('a[href="#"]');
+    enlacesInteractivos.forEach(enlace => {
+        enlace.addEventListener("click", (event) => {
+            event.preventDefault(); // Previene la acción predeterminada
+            console.log("Clic en el enlace sin mover la página.");
+        });
+    });
+});
